1152193f064264effbd7d97cf6bbc353e24ed5fe
ChokoJoestar <mohamedboulidam680@gmail.com>
2023-05-03 20:03:25 +0200

    Add a transformer with chalk

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
    initPackageInstaller,
    initTypescript,
 } from "./function.js";
+import chalk from "chalk";
 
 console.clear();
 console.log(
@@ -21,12 +22,27 @@ inquirer
          type: "input",
          name: "projectDir",
          message: "What is your project name ?",
+         transformer(input, answers, flags) {
+            return chalk.blue(input);
+         },
+      },
+      {
+         type: "input",
+         name: "projectVersion",
+         message: "What is your project version ?",
+         default: "1.0.0",
+         transformer(input, answers, flags) {
+            return chalk.blue(input);
+         },
       },
       {
          type: "input",
          name: "sourceDir",
          message: "Entre your source dir:",
          default: "src",
+         transformer(input, answers, flags) {
+            return chalk.blue(input);
+         },
       },
       {
          type: "list",
@@ -34,12 +50,18 @@ inquirer
          message: "Choise your package installer:",
          choices: ["npm", "yarn"],
          default: "npm",
+         transformer(input, answers, flags) {
+            return chalk.blue(input);
+         },
       },
       {
          type: "confirm",
          name: "installTypescript",
          message: "Do you want to install typescript ?",
          default: true,
+         transformer(input, answers, flags) {
+            return chalk.blue(input);
+         },
       },
    ])
    .then(async (responce: any) => {
